2181afe84f97a1d10ce13c0a26f7619d0965709b
nectia <nectia@10.9.206.112>
2018-10-29 20:11:41 -0500

    Mi primer Commit

--- Diseno_De_Software/src/voraces/divide_y_vencer.java
+++ Diseno_De_Software/src/voraces/divide_y_vencer.java
@@ -1,49 +1,51 @@
 package voraces;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class divide_y_vencer {
 
-	// Formula
-	public static double f(double x) {
-		return Math.pow(x, 3) + (2 * Math.pow(x, 2)) - 5;
-	}
+		// Formula
+		public static double f(double x) {
+			return Math.pow(x, 3) + (2 * Math.pow(x, 2)) - 5;
+		}
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 
-		// Imports
-		Scanner sc = new Scanner(System.in);
+			// Imports
+			Scanner sc = new Scanner(System.in);
 
-		// Variables
-		double m = 0;
-		double a = 0;
-		double b = 2;
-		System.out.println("Ingrese el intervalo a:");
-		a = sc.nextInt();
-		System.out.println("Ingrese el intervalo b:");
-		b = sc.nextInt();
+			// Variables
+			double m = 0;
+			double a = 0;
+			double b = 2;
+			System.out.println("Ingrese el intervalo a:");
+			a = sc.nextDouble();
+			System.out.println("Ingrese el intervalo b:");
+			b = sc.nextDouble();
 
-		// Varificar que no son postivos
-		if (f(a) * f(m) > 0) {
+			// Varificar que no son postivos
+			if (f(a) * f(b) > 0) {
+				
+				System.out.println("No es posible localizar un punto medio.");
 
-		} else {
+			} else {
 
-			while (true) {
-				m = (a + b) / 2;
-				if (Math.abs(f(m)) <= 0.001) {
-					System.out.println("Val:" + m);
-					System.out.println("val2:" + f(m));
-					return;
+				while (true) {
+					m = (a + b) / 2;
+					if (Math.abs(f(m)) <= 0.001) {
+						System.out.println("Valor de corte en eje X: " + m);
+						System.out.println("Valor de m en corte:" + f(m));
+						return;
+					}
+					if (f(a) * f(m) < 0) {
+						b = m;
+					} else {
+						a = m;
+					}
 				}
-				if (f(a) * f(m) < 0) {
-					b = m;
-				} else {
-					a = m;
-				}
+
 			}
 
 		}
-
-	}
-
+	
 }
